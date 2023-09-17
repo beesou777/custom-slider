26658911505a27e43b581504f6d103141a29c330
Bishwa Jung Shah <103716649+beesou777@users.noreply.github.com>
2023-09-17 19:44:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const buttonList = document.querySelectorAll("button");
+    const content = document.querySelector(".slides");
+  
+    buttonList.forEach((button) => {
+      button.addEventListener("click", () => {
+        const direction = button.id === "prev" ? -1 : 1;
+        const scrollAmount = (content.clientWidth * direction) / 4;
+        content.scrollBy({ left: scrollAmount, behavior: "smooth" });
+      });
+    });
+  });
+  
